a899bd918d42db3576db2b5c55245cc097f030c4
srujanponnur <srujanponnur@gmail.com>
2019-10-13 09:37:10 +0530

    Add files via upload

--- Bootstrap-Assignment/main.js
+++ Bootstrap-Assignment/main.js
@@ -1,10 +1,46 @@
-var orderHistoryData = [{"orderID":'2',"OrderDesc":"Table2","OrderDelDate":"13-9-2017"},{"orderID":'1',"OrderDesc":"Table3","OrderDelDate":"12-9-2019"},{"orderID":'3',"OrderDesc":"Table1","OrderDelDate":"12-8-2019"}]
+var orderHistoryData = [{"orderID":'2',"OrderDesc":"Table2","OrderDelDate":"2008/02/26"},
+{"orderID":'1',"OrderDesc":"Table1","OrderDelDate":"2008/01/26"},
+{"orderID":'3',"OrderDesc":"Table3","OrderDelDate":"2008/09/26"},
+{"orderID":'23',"OrderDesc":"groceries","OrderDelDate":"2008/09/26"},
+{"orderID":'4',"OrderDesc":"clothes","OrderDelDate":"2008/09/26"},
+{"orderID":'5',"OrderDesc":"accessories","OrderDelDate":"2008/09/26"},
+{"orderID":'6',"OrderDesc":"Table6","OrderDelDate":"2008/09/26"},
+{"orderID":'7',"OrderDesc":"Table7","OrderDelDate":"2008/09/26"},
+{"orderID":'8',"OrderDesc":"Table8","OrderDelDate":"2008/09/26"},
+{"orderID":'9',"OrderDesc":"Table9","OrderDelDate":"2008/09/26"},
+{"orderID":'10',"OrderDesc":"Table10","OrderDelDate":"2008/09/26"},
+{"orderID":'11',"OrderDesc":"Table11","OrderDelDate":"2008/09/26"},
+{"orderID":'12',"OrderDesc":"Table12","OrderDelDate":"2008/09/26"},
+{"orderID":'13',"OrderDesc":"Table13","OrderDelDate":"2008/09/26"},
+{"orderID":'14',"OrderDesc":"Table14","OrderDelDate":"2008/09/26"},
+{"orderID":'15',"OrderDesc":"Table15","OrderDelDate":"2008/09/26"},
+{"orderID":'16',"OrderDesc":"Table16","OrderDelDate":"2008/09/26"},
+{"orderID":'17',"OrderDesc":"Table17","OrderDelDate":"2008/09/26"},
+{"orderID":'18',"OrderDesc":"Table18","OrderDelDate":"2008/09/26"},
+{"orderID":'19',"OrderDesc":"Table19","OrderDelDate":"2008/09/26"},
+{"orderID":'20',"OrderDesc":"Table20","OrderDelDate":"2008/09/26"},
+{"orderID":'21',"OrderDesc":"Table21","OrderDelDate":"2008/09/26"},
+{"orderID":'22',"OrderDesc":"Table21","OrderDelDate":"2008/09/26"},
+{"orderID":'24',"OrderDesc":"Table24","OrderDelDate":"2013/08/26"},
+{"orderID":'25',"OrderDesc":"Table25","OrderDelDate":"2013/09/26"}]
 
 var listData = [{"ItemName":'phone',"ItemQuality":"1","ItemReqDate":"Sep-13-2019"},{"orderName":'Laptop',"ItemQuality":"1","OrderDelDate":"Sep-22-2019"},{"orderID":'1',"OrderDesc":"Table","OrderDelDate":"Sep-12-2019"}]
 
 var tempData = {"ItemName":'',"ItemQuality":"","ItemReqDate":""};
 
 var orderHistoryTable  = document.getElementById('historyTable');
+
+function lol()
+{
+	console.log('lol');
+}
+
+
+$(document).ready(function() {
+	//$.fn.dataTable.moment( 'MM-dd-yyyy' );
+    $('#historyTable').DataTable();
+} );
+
 
 for(var i in orderHistoryData)
 {
@@ -21,7 +57,7 @@ for(var i in orderHistoryData)
 	orderHistoryTbody.appendChild(orderHistoryTr);
 }
 
-orderHistoryTable.appendChild(orderHistoryTbody);
+orderHistoryTable.appendChild(orderHistoryTbody,lol);
 
 for(var i in listData)
 {
@@ -45,6 +81,8 @@ for(var i in listData)
 	listTr.appendChild(listTd);
 	listTable.appendChild(listTr);
 }
+
+
 
 $('#historyTable tr').on('mouseenter',function(){
 	$(this).css({'background-color':'#d9d9d9','color':'white'});
@@ -99,42 +137,42 @@ $('.firstSection,.secondSection,.thirdSection,.fourthSection').click(function()
    
 });
 
-$('#historyTable .table-up').on('click',function(){
-	var value = $(this).parent().attr('value')-1 ;
-	var $tbody = $('#historyBody');
-	var rows = $('#historyBody tr').get();
-	rows.sort(function(a,b){ 
-	    var tda = $(a).find('td:eq('+value+')').text(); 
-	    var tdb = $(b).find('td:eq('+value+')').text();
-	    if(value==2)
-	    {
-	    	tda = Date.parse(tda);
-	    	tdb = Date.parse(tdb);
-	    }
-	    return tda < tdb ? 1 : tda > tdb ? -1  : 0;           
-	});
-	  $.each(rows, function(index, row) {
-	    $('#historyTable').children('tbody').append(row);
-	  });
-})
+// $('#historyTable .table-up').on('click',function(){
+// 	var value = $(this).parent().attr('value')-1 ;
+// 	var $tbody = $('#historyBody');
+// 	var rows = $('#historyBody tr').get();
+// 	rows.sort(function(a,b){ 
+// 	    var tda = $(a).find('td:eq('+value+')').text(); 
+// 	    var tdb = $(b).find('td:eq('+value+')').text();
+// 	    if(value==2)
+// 	    {
+// 	    	tda = Date.parse(tda);
+// 	    	tdb = Date.parse(tdb);
+// 	    }
+// 	    return tda < tdb ? 1 : tda > tdb ? -1  : 0;           
+// 	});
+// 	  $.each(rows, function(index, row) {
+// 	    $('#historyTable').children('tbody').append(row);
+// 	  });
+// })
 
-$('#historyTable .table-down').on('click',function(){
-	var value = $(this).parent().attr('value')-1 ;
-	var $tbody = $('#historyBody');
-	var rows = $('#historyBody tr').get();
-	rows.sort(function(a,b){ 
-	    var tda = $(a).find('td:eq('+value+')').text();
-	    var tdb = $(b).find('td:eq('+value+')').text();
-	    if(value==2)
-	    {
-	    	var tda = Date.parse(tda);
-	    	var tdb = Date.parse(tdb);
-	    }      
-	    return tda < tdb ? -1 : tda > tdb ? 1 : 0;           
-	});
-  $.each(rows, function(index, row) {
-    $('#historyTable').children('tbody').append(row);
-  });
-})
+// $('#historyTable .table-down').on('click',function(){
+// 	var value = $(this).parent().attr('value')-1 ;
+// 	var $tbody = $('#historyBody');
+// 	var rows = $('#historyBody tr').get();
+// 	rows.sort(function(a,b){ 
+// 	    var tda = $(a).find('td:eq('+value+')').text();
+// 	    var tdb = $(b).find('td:eq('+value+')').text();
+// 	    if(value==2)
+// 	    {
+// 	    	var tda = Date.parse(tda);
+// 	    	var tdb = Date.parse(tdb);
+// 	    }      
+// 	    return tda < tdb ? -1 : tda > tdb ? 1 : 0;           
+// 	});
+//   $.each(rows, function(index, row) {
+//     $('#historyTable').children('tbody').append(row);
+//   });
+// })
 
 
