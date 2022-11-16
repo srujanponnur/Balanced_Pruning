8c8c4832116d5ee2af26fd95a04d69074bc41671
Srujan Ponnur <srujanponnur@gmail.com>
2022-11-16 15:38:04 -0500

    changed dataset to imbalanced cifar10

--- pruning.py
+++ pruning.py
@@ -20,6 +20,56 @@ import seaborn as sns
 import torch.nn.init as init
 import pickle
 
+class ImbalanceCIFAR10(torchvision.datasets.CIFAR10):
+    cls_num = 10
+
+    def __init__(self, root, imb_type='exp', imb_factor=0.5, rand_number=0, train=True,
+                 transform=None, target_transform=None, download=False):
+        super(ImbalanceCIFAR10, self).__init__(root, train, transform, target_transform, download)
+        np.random.seed(rand_number)
+        img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imb_factor)
+        self.gen_imbalanced_data(img_num_list)
+
+    def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
+        img_max = len(self.data) / cls_num
+        img_num_per_cls = []
+        if imb_type == 'exp':
+            for cls_idx in range(cls_num):
+                num = img_max * (imb_factor**(cls_idx / (cls_num - 1.0)))
+                img_num_per_cls.append(int(num))
+        elif imb_type == 'step':
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max))
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max * imb_factor))
+        else:
+            img_num_per_cls.extend([int(img_max)] * cls_num)
+        return img_num_per_cls
+
+    def gen_imbalanced_data(self, img_num_per_cls):
+        new_data = []
+        new_targets = []
+        targets_np = np.array(self.targets, dtype=np.int64)
+        classes = np.unique(targets_np)
+        # np.random.shuffle(classes)
+        self.num_per_cls_dict = dict()
+        for the_class, the_img_num in zip(classes, img_num_per_cls):
+            self.num_per_cls_dict[the_class] = the_img_num
+            idx = np.where(targets_np == the_class)[0]
+            np.random.shuffle(idx)
+            selec_idx = idx[:the_img_num]
+            new_data.append(self.data[selec_idx, ...])
+            new_targets.extend([the_class, ] * the_img_num)
+        new_data = np.vstack(new_data)
+        self.data = new_data
+        self.targets = new_targets
+        
+    def get_cls_num_list(self):
+        cls_num_list = []
+        for i in range(self.cls_num):
+            cls_num_list.append(self.num_per_cls_dict[i])
+        return cls_num_list
+
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -280,7 +330,8 @@ def test(model, test_loader, criterion):
     return accuracy
 
 transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))])
-traindataset = datasets.CIFAR10('../data', train=True, download=True,transform=transform)
+# traindataset = datasets.CIFAR10('../data', train=True, download=True,transform=transform) # this is for balance
+traindataset = ImbalanceCIFAR10('../data',train=True,download=True, transform=transform)  # this is for imbalance
 testdataset = datasets.CIFAR10('../data', train=False, transform=transform)
 
 train_loader = torch.utils.data.DataLoader(traindataset, batch_size=60, shuffle=True, num_workers=0,drop_last=False)
